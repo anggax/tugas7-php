783fea49df0800f9c681434ac97a23b873122072
anggax <73506030+anggax@users.noreply.github.com>
2022-05-10 07:03:20 +0700

    Add files via upload

---
+++ no 2-3/tugas3hapus.php
@@ -0,0 +1,8 @@
+<?php
+include 'koneksi.php';
+$id = $_GET['id'];
+
+mysqli_query($koneksi, "DELETE FROM `db_fakultas` WHERE `db_fakultas`.`id` = $id");
+
+header("location: tugas3index.php");
+?>
---
+++ no 2-3/tugas3tambah.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CRUD PHP</title>
+</head>
+<body>
+    <center>
+    <h2> CRUD DATA PEGAWAI</h2>
+    <br>
+	<a href="tugas3index.php">KEMBALI</a>
+	<br/>
+	<br/>
+	<h3>TAMBAH DATA Pegawai</h3>
+	<form method="post" action="tugas3tambahact.php">
+		<table>
+			<tr >			
+				<td width = "300">Nama : </td>
+				<td width = "300"><input type="text" name="nama"></td>
+			</tr>
+			<tr>
+				<td width = "300">Email : </td>
+				<td width = "300"><input type="text" name="mail"></td>
+			</tr>
+			<tr>
+				<td width = "300">Alamat</td>
+				<td width = "300"><input type="text" name="alamat"></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td ><input type="submit" value="SIMPAN"></td>
+				<td></td>
+            </tr>		
+		</table>
+	</form>
+    </center>
+</body>
+</html>
